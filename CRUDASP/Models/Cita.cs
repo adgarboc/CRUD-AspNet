1f481cdf824ac8c89cd6b17c9d59fe2e1289b925
Adán García <adgarboc@gmail.com>
2021-06-15 22:22:01 -0500

    Agregar archivos de proyecto.

---
+++ CRUDASP/Models/Cita.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUDASP.Models
+{
+    public class Cita
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [Display(Name = "Titulo")]
+        public string Titulo { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "Fecha")]
+        public DateTime Fecha { get; set; }
+
+        [Required]
+        [DataType(DataType.Time)]
+        [Display(Name = "Hora")]
+        public DateTime Hora { get; set; }
+    }
+}
