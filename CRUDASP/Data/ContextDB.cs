624dc38857e15ec1dc9e6a64c028940046651170
Adán García <adgarboc@gmail.com>
2021-06-15 22:38:26 -0500

    Configurado Db context

---
+++ CRUDASP/Data/ContextDB.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CRUDASP.Models;
+
+namespace CRUDASP.Data
+{
+    public class ContextDB:DbContext
+    {
+        public ContextDB(DbContextOptions<ContextDB> options):base(options)
+        {
+            
+        }
+
+        public DbSet<Cita> Citas { get; set; }
+    }
+}
--- CRUDASP/Startup.cs
+++ CRUDASP/Startup.cs
@@ -8,6 +8,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CRUDASP.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace CRUDASP
 {
@@ -23,6 +25,9 @@ namespace CRUDASP
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ContextDB>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddControllersWithViews();
         }
 
